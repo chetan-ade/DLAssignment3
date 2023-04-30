6819f630d64d856f2a743ea884e1ac7bfe2c7e85
Chetan Ade <chetan.ade98@gmail.com>
2023-04-30 14:48:36 +0530

    Broke Down Evaluate into evaluate() and evaluateBatch()

--- train.py
+++ train.py
@@ -8,15 +8,34 @@ import torch
 from dataProcessing import DataProcessing
 import main
 
+def sweepTrain():
+    
+    wandb.init(config = sweepConfigs)
+
+    configs["hiddenSize"] = wandb.config.hidden_size
+    configs["cellType"] = wandb.config.cell_type
+    configs["numLayersEncoderDecoder"] = wandb.config.num_layers
+    configs["dropout"] = wandb.config.drop_out
+    configs["embeddingSize"] = wandb.config.embedding_size
+    configs["bidirectional"] = wandb.config.bidirectional
+    configs["batchSize"] = wandb.config.batch_size
+    configs["epochs"] = wandb.config.epoch
+    configs["learningRate"] = wandb.config.learning_rate
+    configs["attention"] = wandb.config.attention
+    
+    wandb.run.name = "cell_type_" + str(wandb.config.cell_type) +  "_numLayers_"+ str(wandb.config.num_layers) + "_attention_" + str(wandb.config.attention)   
+    
+    main.trainForConfigs(configs, dataProcessor) # Train for configurations
+    
 if __name__ == "__main__" :
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dataProcessor = DataProcessing('aksharantar_sampled', 'hin', device) # Pre-Process the data
 
-    # main.trainConfigsGrid(device, dataProcessor)
-
     configs = { # Create a configuration dictionary
-    
+
+        'wandb_project'             : 'DL Assignment 3',
+        'wandb_entity'              : 'cs22m033',
         'device'                    : device,  # Available Device (CPU / CUDA) 
         'hiddenSize'                : 64,     # Hidden Size in RNN Layer
         'cellType'                  : "LSTM",   # Cell Type = ['RNN', 'GRU', 'LSTM']
@@ -31,4 +50,58 @@ if __name__ == "__main__" :
 
     }
 
-    main.trainForConfigs(configs, dataProcessor) # Train for configurations
+    sweepConfigs = {
+        'method' : 'bayes',
+        'name' : 'bayes_sweep',
+
+        'metric' : {
+            'name' : 'validation accuracy',
+            'goal' : 'maximize'
+        },
+
+        'epoch':{
+            'values' : [10, 20]
+        },
+
+        'hidden_size':{
+            'values' : [64,128,256]
+        },
+
+        'cell_type':{
+            'values' : ['lstm','rnn','gru']
+        },
+
+        'learning_rate':{
+            'values' : [1e-2,1e-3]
+        },
+
+        'num_layers':{
+            'values' : [1,2,3]
+        },
+
+        'drop_out':{
+            'values' : [0.0,0.2,0.3]
+        },
+
+        'embedding_size':{
+            'values' : [64,128,256]
+        },
+
+        'batch_size':{
+            'values' : [32,64, 128]
+        },
+        'bidirectional':{
+            'values' : [True,False]
+        },
+        'attention':{
+            'values' : [False]
+        }
+        
+    }
+
+    sweep_id = wandb.sweep(sweep = sweepConfigs,project= configs["wandb_project"])
+    wandb.agent(sweep_id=sweep_id,function = sweepTrain)   # Start agent 
+        
+    
+
+   
--- trainModel.py
+++ trainModel.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals, print_function, division
 import torch
 import random
+import wandb
 
 import torch
 import torch.nn as nn
@@ -146,9 +147,6 @@ class Training :
         trainLoader = DataLoader(self.dataProcessor.trainingTensorPairs, batch_size = self.batchSize, shuffle = True)
         validLoader = DataLoader(self.dataProcessor.validTensorPairs, batch_size = self.batchSize, shuffle = True)
         
-        plot_losses = []
-        plot_loss_total = 0  # Reset every plotEvery
-
         for epoch in range(self.epochs) :
 
             epochLoss = 0 
@@ -161,267 +159,117 @@ class Training :
                 targetTensor = targetTensor.transpose(0, 1)
 
                 batchLoss = self.trainOneBatch(sourceTensor, targetTensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
-
-                # Store the loss for printing and plotting
                 epochLoss += batchLoss
-                plot_loss_total += batchLoss
-
                 batchNumber += 1
 
                 if (batchNumber % (totalBatches // 10)) == 0 :
                     print('Loss For Batch ' + str(batchNumber) + '/' + str(totalBatches) + ' : ' + str(batchLoss))
 
             epochLoss /= totalBatches
+            # validationLoss, validationAccuracy = self.evaluate(validLoader, encoder, decoder)
+            validationLoss, validationAccuracy = self.evaluate(validLoader, encoder, decoder)
 
             print('Loss For Epoch ' + str(epoch + 1) + '/' + str(self.epochs) + ' : ' + str(epochLoss) + '\n')
+            print('Validation Loss : ', validationLoss)
+            print(' Validation Accuracy :', validationAccuracy)
 
-            # print(' Train Accuracy :', self.evaluate(trainLoader, encoder, decoder))
-            print(' Validation Accuracy :', self.evaluate(validLoader, encoder, decoder))
-
-        # Plot the list 
-        self.showPlot(plot_losses)
-
-    def showPlot(self, points):
-        plt.figure()
-        fig, ax = plt.subplots()
-
-        # this locator puts ticks at regular intervals
-        loc = ticker.MultipleLocator(base=0.2)
-        ax.yaxis.set_major_locator(loc)
-        plt.plot(points)
-        # plt.show()
-
+            # wandb.log({'train loss':epochLoss,'validation loss':validationLoss, 'validation accuracy':validationAccuracy})
+    
     def evaluate(self, loader, encoder, decoder) :
-    
-        batchSize = self.dataProcessor.batchSize
-        device = self.dataProcessor.device
-        maxLengthWord = self.dataProcessor.maxLengthWord 
-        
-        totalWords = 0
-        correctWords = 0
+
+        loss = 0
+        totalCorrectWords = 0
         batchNumber = 1
-        totalBatches = len(loader.sampler) // batchSize
-            
-        for sourceTensor, targetTensor in loader:
-            
-            source_tensor = Variable(sourceTensor.transpose(0, 1))
-            target_tensor = Variable( targetTensor.transpose(0, 1))
-            
-            # Get source length
-            source_length = source_tensor.size()[0]
-            target_length = target_tensor.size()[0]
-            output = torch.LongTensor(target_length, batchSize)
+        
+        totalWords = len(loader.sampler)
+        totalBatches = len(loader.sampler) // self.dataProcessor.batchSize
 
-            # Initialize initial hidden state of encoder
-            encoder_hidden = encoder.initHidden()
+        # Loss Function
+        criterion = nn.NLLLoss()
 
-            if self.dataProcessor.cellType == "LSTM":
-                encoder_cell_state = encoder.initHidden()
-                encoder_hidden = (encoder_hidden,encoder_cell_state)
-    
-            if self.attention:
-                encoder_outputs = torch.zeros(maxLengthWord + 1, batchSize, encoder.hiddenSize, device = device)
+        for sourceTensor, targetTensor in loader :
+            batchLoss, correctWords = self.evaluateOneBatch(sourceTensor, targetTensor, encoder, decoder, criterion)
 
-            for ei in range(source_length):
-                encoder_output, encoder_hidden = encoder(source_tensor[ei],encoder_hidden)
+            loss += batchLoss
+            totalCorrectWords += correctWords
 
-                if self.attention :
-                    # encoderOutputs[sourceIndex] = encoderOutput[0] # ? Other Code
-                    encoder_outputs[ei] = encoder_output[0]
-
-            # Initialize input to decoder with start of word token
-            decoder_input = torch.tensor([[self.dataProcessor.SOW2Int] * batchSize], device = device)
-
-            # initial hidden state for decoder will be final hidden state of encoder
-            decoder_hidden = encoder_hidden
-
-            if self.attention :
-                decoderAttentions = torch.zeros(self.maxLengthTensor, self.maxLengthTensor)
-            
-            for di in range(target_length):
-
-                if self.attention :
-                    decoder_output, decoder_hidden, decoderAttention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-                    decoderAttentions[di] = decoderAttention.data
-
-                else : 
-                    # Pass the decoder_input, decoder_hidden and encoderOutputs to the decoder
-                    decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-                
-                topv, topi = decoder_output.data.topk(1)
-                decoder_input = torch.cat(tuple(topi))
-                output[di] = torch.cat(tuple(topi))
-
-            if False :
-                plt.matshow(decoderAttentions[:di + 1].numpy())
-
-            output = output.transpose(0,1)
-            
-            for di in range(output.size()[0]):
-                ignore = [self.dataProcessor.SOW2Int, self.dataProcessor.EOW2Int, self.dataProcessor.PAD2Int]
-                predicted = [letter.item() for letter in output[di] if letter not in ignore]
-                actual = [letter.item() for letter in  targetTensor[di] if letter not in ignore]
-                # print("predicted:",predicted)
-                # print("=actual:",actual)
-                if predicted == actual:
-                    correctWords += 1
-                totalWords += 1
             print("Evaluate Batch : " + str(batchNumber) + "/" + str(totalBatches))
-            batchNumber+=1
-        # print('accuracy '+ str((correct/total)*100))
-        return (correctWords/totalWords)*100
+            batchNumber += 1 
 
+        return (loss / totalBatches), (totalCorrectWords / totalWords)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def evaluateBatch(self, sourceTensor, targetTensor, encoder, decoder, criterion) :
-
-        '''
-        Input :
-                encoder : Encoder Object
-                decoder : Decoder Object
-                word : Input Word to be transliterated
-                maxLength : TODO
-        '''
+    def evaluateOneBatch(self, sourceTensorBatch, targetTensorBatch, encoder, decoder, criterion) :
 
         loss = 0
         correctWords = 0
 
-        with torch.no_grad():
+        batchSize = self.dataProcessor.batchSize
+        device = self.dataProcessor.device
+        maxLengthWord = self.dataProcessor.maxLengthWord 
+        
+        sourceTensor = Variable(sourceTensorBatch.transpose(0, 1))
+        targetTensor = Variable(targetTensorBatch.transpose(0, 1))
+        
+        # Get source length
+        sourceTensorLength = sourceTensor.size()[0]
+        targetTensorLength = targetTensor.size()[0]
 
-            sourceTensor = sourceTensor.squeeze()
-            targetTensor = targetTensor.squeeze()
+        predictedBatchOutput = torch.zeros(targetTensorLength, batchSize, device = self.dataProcessor.device)
 
-            # Length of Source Tensor
-            sourceTensorLength = sourceTensor.size()[0]
+        # Initialize initial hidden state of encoder
+        encoderHidden = encoder.initHidden()
 
-            # Length of target Tensor
-            targetTensorLength = targetTensor.size()[0]
+        if self.dataProcessor.cellType == "LSTM":
+            encoderCell = encoder.initHidden()
+            encoderHidden = (encoderHidden, encoderCell)
 
-            # Initialize the first hidden state for encoder
-            encoderHidden = encoder.initHidden()
+        if self.attention:
+            encoderOutputs = torch.zeros(maxLengthWord + 1, batchSize, encoder.hiddenSize, device = device)
 
-            if self.dataProcessor.cellType == 'LSTM' :
-                encoderCell = encoder.initCell()
-                encoderHidden = (encoderHidden, encoderCell)
-
-            if self.attention :
-                encoderOutputs = torch.zeros(self.maxLengthTensor, self.dataProcessor.batchSize, encoder.hiddenSize, device = self.dataProcessor.device)
-
-            output = torch.LongTensor(targetTensorLength, self.dataProcessor.batchSize)
-
-            # Iterate over the sourceTensor
-            for sourceIndex in range(sourceTensorLength) :
-                
-                # Pass the charIndex present at sourceTensor[sourceIndex] as input tensor
-                encoderOutput, encoderHidden = encoder(sourceTensor[sourceIndex], encoderHidden)
-
-                if self.attention : 
-                    
-                    # Stores the encoder output for current source character in encoderOutputs
-                    # encoderOutputs[sourceIndex] = encoderOutput[0] # ? Other Code
-                    encoderOutputs[sourceIndex] = encoderOutput[0]
-
-            # Give SOW (Start of Word) as Decoder Input
-            decoderInput = torch.tensor([[self.dataProcessor.SOW2Int] * self.batchSize], device = self.dataProcessor.device)
-
-            # The hidden state input to decoder is the final hidden state output of encoder
-            decoderHidden = encoderHidden
+        for ei in range(sourceTensorLength):
+            encoderOutput, encoderHidden = encoder(sourceTensor[ei], encoderHidden)
 
             if self.attention :
-                decoderAttentions = torch.zeros(self.maxLengthTensor, self.maxLengthTensor)
+                # encoderOutputs[sourceIndex] = encoderOutput[0] # ? Other Code
+                encoderOutputs[ei] = encoderOutput[0]
 
-            for targetIndex in range(targetTensorLength):
-                
-                if self.attention :
-                    decoderOutput, decoderHidden, decoderAttention = decoder(decoderInput, decoderHidden, encoderOutputs)
+        # Initialize input to decoder with start of word token
+        decoderInput = torch.tensor([[self.dataProcessor.SOW2Int] * batchSize], device = device)
 
-                else : 
-                    # Pass the decoderInput, decoderHidden and encoderOutputs to the decoder
-                    decoderOutput, decoderHidden = decoder(decoderInput, decoderHidden)
-                
-                if self.attention :
-                    decoderAttentions[targetIndex] = decoderAttention.data
-                
-                # Since the output of decoder is softmax, we want the value and index of maximum probability
-                topv, topi = decoderOutput.data.topk(1)
-                loss += criterion(decoderOutput, targetTensor[targetIndex])
+        # initial hidden state for decoder will be final hidden state of encoder
+        decoderHidden = encoderHidden
 
-                # Squeeze removes all dimensions that are 1 from tensor
-                # Detach creates a new tensor that is not the part of history graph
-                decoderInput = topi.squeeze().detach()
-                output[targetIndex] = decoderInput
+        if self.attention :
+            decoderAttentions = torch.zeros(self.maxLengthTensor, self.maxLengthTensor)
+        
+        for di in range(targetTensorLength):
 
+            if self.attention :
+                decoderOutput, decoderHidden, decoderAttention = decoder(decoderInput, decoderHidden, encoderOutputs)
+                decoderAttentions[di] = decoderAttention.data
+
+            else : 
+                # Pass the decoderInput, decoderHidden and encoderOutputs to the decoder
+                decoderOutput, decoderHidden = decoder(decoderInput, decoderHidden)
+            
+            topv, topi = decoderOutput.data.topk(1)
+            decoderInput = torch.cat(tuple(topi))
+            predictedBatchOutput[di] = torch.cat(tuple(topi))
+
+        if self.attention :
             if False :
                 plt.matshow(decoderAttentions[:di + 1].numpy())
 
-            output = output.transpose(0, 1)
-            print(output.shape)
-
-            print("OUTPUT SIZE : ", output.size()[0])
-            for batchNumber in range(output.size()[0]) :
-                ignore = [self.dataProcessor.SOW2Int, self.dataProcessor.EOW2Int, self.dataProcessor.PAD2Int]
-                predicted = [self.dataProcessor.targetIntToChar[letter.item()] for letter in output[batchNumber] if letter not in ignore]
-
-                print("Length of target tensor : ", targetTensor[batchNumber].shape)
-                actual = [self.dataProcessor.targetIntToChar[letter.item()] for letter in targetTensor[batchNumber] if letter not in ignore]
-
-                if predicted == actual :
-                    correctWords += 1
-            
-            # Return loss and correctWord = 1
-            return loss.item() / targetTensorLength , correctWords
+        predictedBatchOutput = predictedBatchOutput.transpose(0,1)
         
-    def evaluateSplit(self, split, encoder, decoder, criterion) :
+        ignore = [self.dataProcessor.SOW2Int, self.dataProcessor.EOW2Int, self.dataProcessor.PAD2Int]
+
+        for di in range(predictedBatchOutput.size()[0]):
+
+            predicted = [letter.item() for letter in predictedBatchOutput[di] if letter not in ignore]
+            actual = [letter.item() for letter in targetTensorBatch[di] if letter not in ignore]
+
+            if predicted == actual:
+                correctWords += 1
         
-        # List of tuple of source words and train target words
-        if split.lower() == "validation" :
-            tensorPairs = self.dataProcessor.validTensorPairs
-
-        else :
-            tensorPairs = self.dataProcessor.testTensorPairs
-
-        loader = DataLoader(tensorPairs, batch_size = self.batchSize, shuffle = True)
-
-        # Initialize Total Loss for Validation Data
-        totalLoss = 0
-
-        # Initialize Number of Correct Words
-        correctWords = 0
-
-        batchNumber = 0
-        totalBatches = len(tensorPairs) // self.batchSize
-
-        for sourceTensor, targetTensor in loader :
-
-            sourceTensor = sourceTensor.transpose(0, 1)
-            targetTensor = targetTensor.transpose(0, 1)
-
-            batchLoss, correctWordsBatch = self.evaluateBatch(sourceTensor, targetTensor, encoder, decoder, criterion)
-
-            totalLoss += batchLoss
-            correctWords += correctWordsBatch
-            batchNumber += 1
-
-        avgLoss = totalLoss / totalBatches
-        accuracy = correctWords / len(tensorPairs)
-
-        print(split, "Loss :", avgLoss)
-        print(split, "Accuracy :", accuracy)
-
-        return avgLoss, accuracy
-
+        return loss, correctWords
